3e8499110625943cf0f2d59d01a7f9f4b6e0adbd
Harald Walker <hwalker@tripolis.com>
2013-03-23 20:19:16 +0100

    Add documentation

--- lib/ua-utils/operating_system.rb
+++ lib/ua-utils/operating_system.rb
@@ -1,41 +1,62 @@
 module UaUtils
 
-  class OperatingSystem
+  # The operating system that can be detected based on the user agent string.
+  #
+  # === Example
+  #
+  #   os = UaUtils::OperatingSystem.new(
+  #     'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; en-us) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4')
+  #
+  #   if os.tablet?
+  #    (... do something for tablet devices ...)
+  #   end
+  #
+  #   if os.platform == :ios
+  #     render "/ios/show"
+  #   end
 
-    # platform, e.g. 'Windows'
-    # name, e.g. 'Windows 8'
-    # version, e.g. '8.0.1'
-    # device, e.g. :desktop
+  class OperatingSystem
 
     attr_reader :platform, :device
 
+    # Initializes operating system for parsing with the given user agent string.
     def initialize(user_agent_string)
       @platform, @device = detect_device(user_agent_string)
     end
 
+    # Returns string representation of the operating system.
+    # @return [String]
     def to_s
       @platform.to_s
     end
 
+    # Returns true if the operating system is a mobile device.
+    # @return [Boolean]
     def mobile?
       @device == :mobile
     end
 
+    # Returns true if the operating system is a tablet device.
+    # @return [Boolean]
     def tablet?
       @device == :tablet
     end
 
+    # Returns true if the operating system is a desktop device.
+    # @return [Boolean]
     def desktop?
       @device == :desktop
     end
 
+    # Returns true if the operating system is a webTV device.
+    # @return [Boolean]
     def webtv?
       @device == :webtv
     end
 
     private
 
-      def detect_device(ua_string)
+      def detect_device(ua_string) #:nodoc:
         case ua_string
           when /(windows nt).*(touch)/i                         ; return :windows, :tablet
           when /windows nt/i                                    ; return :windows, :desktop
--- lib/ua-utils/user_agent.rb
+++ lib/ua-utils/user_agent.rb
@@ -1,15 +1,34 @@
 module UaUtils
+
+  # The user agent object.
+  #
+  # === Example
+  #
+  #   user_agent = UaUtils::UserAgent.new(
+  #     'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; en-us) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4')
+  #
+  #   if user_agent.os.tablet?
+  #    (... do something for tablet devices ...)
+  #   end
+  #
+  #   if user_agent.os.platform == :ios
+  #     render "/ios/show"
+  #   end
+  #
   class UserAgent
 
     attr_reader :os
 
+    # Initializes a user agent for parsing with a given user agent string
     # @param [Object] user_agent_string
     def initialize(user_agent_string)
       @user_agent_string = user_agent_string
     end
 
+    # Returns operating system of the user agent.
+    # @return [UaUtils::OperatingSystem] the detected operating system of the user agent string
     def os
-      @os ||= UaUtils::OperatingSystem.new(@user_agent_string)
+      @os ||= UaUtils::OperatingSystem.new(@user_agent_string) # operating system
     end
 
   end
